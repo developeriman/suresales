d9e2b9293c4e1688d5b21cec497defce3ccd051c
ammasum <abdullamuhammed76@gmail.com>
2023-02-20 19:40:40 +0600

    template logic

--- public/admin/js/template/template.js
+++ public/admin/js/template/template.js
@@ -1,84 +1,71 @@
 var makeContact = [];
-var slno = 0;
+var slno = 1;
 var checkfield = -1;
 $(document).ready(function() {
-		console.log('here')
-		addmore();
+    $('#generatecode').click(function() {
+        var templatename = $('#templatename').val();
+        var prefix = $('#prefix').val();
+        var noofcodes = $('#noofcodes').val();
 
-		$('#generatecode').click(function() 
-		{
-		//alert('addrow')
-		    var templatename = $('#templatename').val();
-			var prefix = $('#prefix').val();
-			var noofcodes = $('#noofcodes').val();
-			
-		    var _token = $('input[name="_token"]').val();
+        var _token = $('input[name="_token"]').val();
 
-			$.post('./generatecode', {'templatename': templatename,'prefix':prefix,'noofcodes':noofcodes,'_token':_token}, function (data) {
+        $.post('./generatecode', {'templatename': templatename,'prefix':prefix,'noofcodes':noofcodes,'_token':_token}, function (data) {
 
-               console.log('data.....'+ data)
-			
+           console.log('data.....'+ data)
 
-            });
 
         });
 
-		$('#savetemplate').click(function() 
-		{
-		//alert('addrow')
-		    var _token = $('input[name="_token"]').val();
-			for(io = 1; io<=slno; io++)
-			{
-			AddProduct(io);
-			}
-			$.post('./templatesave', {'jr': makeContact,'_token':_token}, function (data) {
+    });
 
-               console.log('data.....'+ data)
-				slno = 0;
-				$('#appendjournal').html('');
+    $('#savetemplate').click(function() {
+        var _token = $('input[name="_token"]').val();
+        const data = bindTemplate();
+        $.post('./templatesave', {data,'_token':_token}, function (data) {
 
-            });
-
+            console.log('data.....'+ data)
+            slno = 0;
         });
-	
 
-		$('#addrow').click(function() 
-		{
-		//alert('addrow')
-		slno++;
-		$('#appendrow').append('<div  class="mb-3"  id="data' + slno + '"><table id="customers"><tr><td><b>Title</b></td><td><b>Type</b></td></tr><tr><td>First Name</td><td><input type="text" class="form-control" onkeyup="setFullname(' + slno + ', ' + slno + ')" id="FirstName' + slno + '" value=""></td></tr><tr><td>Last Name</td><td><input type="text" class="form-control" onkeyup="setFullname(' + slno + ', ' + slno + ')" id="LastName' + slno + '" value=""></td></tr><tr><td>Full Name</td><td><input type="text" class="form-control" id="FullName' + slno + '" value=""></td></tr><tr><td>Phone</td><td><input type="text" class="form-control" id="Phone' + slno + '" value=""></td></tr><tr><td>Picture</td><td><input type="file" class="form-control" id="picture' + slno + '" value=""></td><tr> <tr><td></td><td><button type="submit" class="btn btn-danger" style="font-size: 12px" onClick=\'removerow("' + slno + '")\'>+ Delete Row</button></td></tr></table></div');
-		});
+    });
 
-		});
-		function addmore()
-		{
-		slno++;
-		$('#appendrow').append('<div class="mb-3" id="data' + slno + '"><table id="customers"><tr><td><b>Title</b></td><td><b>Type</b></td></tr><tr><td>First Name</td><td><input type="text" class="form-control" onkeyup="setFullname(' + slno + ', ' + slno + ')" id="FirstName' + slno + '" value=""></td></tr><tr><td>Last Name</td><td><input type="text" class="form-control" onkeyup="setFullname(' + slno + ', ' + slno + ')" id="LastName' + slno + '" value=""></td></tr><tr><td>Full Name</td><td><input type="text" class="form-control" id="FullName' + slno + '" value=""></td></tr><tr><td>Phone</td><td><input type="text" class="form-control" id="Phone' + slno + '" value=""></td></tr><tr><td>Picture</td><td><input type="file" class="form-control" id="picture' + slno + '" value=""></td><tr> <tr><td><td style="text-align: right"><button type="submit" class="btn btn-danger" style="font-size: 12px" onClick=\'removerow("' + slno + '")\'>+ Delete Row</button></td></tr></table></div');
-	
-	//	$('#appendrow').append('<div class="col-md-3" id="data' + slno + '"><div><input type="text" class="form-control" onkeyup="setFullname(' + slno + ', ' + slno + ')"  id="FirstName' + slno + '" value=""></div><div><input type="text" class="form-control" onkeyup="setFullname(' + slno + ', ' + slno + ')" id="LastName' + slno + '" value=""></div><div><input type="text" class="form-control" id="FullName' + slno + '" value=""></div><div><input type="text" class="form-control" id="Phone' + slno + '" value=""></div><div><input type="file" class="form-control" id="picture' + slno + '" value=""></div></div>');
+    $('#addrow').click(function() {
+        slno++;
+        $('#appendrow').append(`
+            <tr id="template-row-${slno}">
+                <td><input type="text" class="form-control" id="template-input-${slno}" value=""></td>
+                <td>
+                    <select class="form-select" value="text" id="template-input-type-${slno}">
+                        <option value="text">Text</option>
+                        <option value="number">Number</option>
+                        <option value="image">Image</option>
+                    </select>
+                </td>
+            </tr>
+        `);
 
-		}
-		function removerow(x)
-		{
+    });
 
-		$('#data' + x).remove();
-		slno--;
-		}
-		function setFullname(x)
-		{
-			var m = $('#FirstName' + x).val();
-			var n = $('#LastName' + x).val();
-			var fullname = m + ' ' +n;
-			$('#FullName' + x).val(fullname);
-		}
-function AddProduct(x)
+});
+
+function removerow() {
+    $(`#template-row-${slno}`).remove();
+    slno--;
+}
+
+function bindTemplate()
 {
-var FirstName = $('#FirstName'+x).val();
-var LastName = $('#LastName'+x).val();
-var FullName = $('#FullName'+x).val();
-var Phone = $('#Phone'+x).val();
-var picture = $('#picture'+x).val();
+    const template = [];
+    for(let i = 1; i <= slno; i++) {
+        var input = $(`#template-input-${i}`).val();
+        var type = $(`#template-input-type-${i}`).val();
 
- makeContact.push([FirstName, LastName, FullName, Phone, picture]);
-  
+        template.push({
+            input,
+            type
+        })
+    }
+
+    return template;
+
 }
